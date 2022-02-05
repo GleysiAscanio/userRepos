04fd98409ce39731a87d2beb012e8c131e3c8865
Gleysi Ascanio <gleysiascanio29@gmail.com>
2022-02-05 13:44:05 -0500

    Component is created and connected to the API, functional to obtain information and display it, styles are also added

---
+++ src/Components/ListRepositories.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material/";
+import dataGithub from "../tokens.jsx";
+
+const bodySearch = {
+  query: `
+              query { 
+                  user(login: "${dataGithub.username}"){
+                    repositories(last:10, orderBy: {field: CREATED_AT, direction:ASC}){
+                      nodes{
+                        id,
+                        name,
+                        description,
+                      }
+                    }
+                  }
+                }`,
+};
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "bearer " + dataGithub.token,
+};
+
+const ListRepositories = () => {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const peticion = await fetch("https://api.github.com/graphql", {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(bodySearch),
+        });
+        const datos = await peticion.json();
+        const trueData = datos.data.user.repositories.nodes;
+        setInfo(trueData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <TableContainer component={Paper} elevation={3}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell
+              align="center"
+              sx={{ fontSize: "26px", fontWeight: "600" }}
+            >
+              Repositorio
+            </TableCell>
+            <TableCell
+              align="center"
+              sx={{ fontSize: "26px", fontWeight: "600" }}
+            >
+              Descripción
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {info.map((dato) => (
+            <TableRow
+              key={dato.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell align="center" sx={{ fontSize: "20px" }}>
+                {dato.name}
+              </TableCell>
+              <TableCell align="center" sx={{ fontSize: "20px" }}>
+                {dato.description
+                  ? dato.description
+                  : " Este Repositorio no posee descripción"}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default ListRepositories;
