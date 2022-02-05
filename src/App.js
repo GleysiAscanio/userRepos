fc8162e3fc793bef093c3a8a528ab190e41b65af
Gleysi Ascanio <gleysiascanio29@gmail.com>
2022-02-05 01:50:39 -0500

    React router is installed and the routes are made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLogin from "./Components/UserLogin.jsx";
+import UserRegister from "./Components/UserRegister.jsx";
 
 function App() {
   return (
     <div className="App">
-      <UserLogin />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserLogin />} />
+          <Route path="/user-register" element={<UserRegister />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
