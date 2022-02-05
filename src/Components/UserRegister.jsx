eee1721aa1ee3c722a26b4ec9252e848ade835f9
Gleysi Ascanio <gleysiascanio29@gmail.com>
2022-02-05 18:46:35 -0500

    They finish adding functionality at the time of registration

--- src/Components/UserRegister.jsx
+++ src/Components/UserRegister.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Container,
   Paper,
@@ -14,26 +14,30 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 const UserRegister = () => {
   // Estados iniciales de los campos y los alerts
-  const [emailUser, setEmailUser] = useState("");
+  const [userName, setUserName] = useState("");
   const [passwordUser, setPasswordUser] = useState("");
   const [openAlertGreen, setOpenAlertGreen] = useState(false);
   const [openAlertRed, setOpenAlertRed] = useState(false);
+  const navigate = useNavigate();
 
   //Evento para el boton de Sign Up - valida campos llenos, guarda en localStorage y activa alerta segun caso
-  const buttonSubmit = (e, setEmailUser, setPasswordUser) => {
+  const buttonSubmit = (e, setUserName, setPasswordUser) => {
     e.preventDefault();
-    if (emailUser === "" && passwordUser === "") {
+    if (userName === "" || passwordUser === "") {
       handleClickRed();
     } else {
-      window.localStorage.setItem("setEmailUser", setEmailUser);
+      window.localStorage.setItem("setUserName", setUserName);
       window.localStorage.setItem("setPasswordUser", setPasswordUser);
       handleClickGreen();
+      setTimeout(() => {
+        navigate("/user-login");
+      }, 3000);
     }
   };
 
   // tomando el valor del input email y cambiando el estado
   const valueinputEmail = (e) => {
-    setEmailUser(e.target.value);
+    setUserName(e.target.value);
   };
 
   // tomando el valor del input password y cambiando el estado
@@ -108,14 +112,14 @@ const UserRegister = () => {
             required
             id="inputCreateUser"
             label="Usuario GitHub"
-            value={emailUser}
+            value={userName}
             sx={{ width: "400px" }}
             onChange={valueinputEmail}
           />
           <TextField
             required
             id="passwordInput"
-            label="Password"
+            label="Token Github"
             value={passwordUser}
             type="password"
             autoComplete="current-password"
@@ -130,7 +134,7 @@ const UserRegister = () => {
               marginTop: 5,
               width: "200px",
             }}
-            onClick={(e) => buttonSubmit(e, emailUser, passwordUser)}
+            onClick={(e) => buttonSubmit(e, userName, passwordUser)}
           >
             Sign Up
           </Button>
@@ -144,7 +148,7 @@ const UserRegister = () => {
               severity="error"
               sx={{ width: "100%" }}
             >
-              Por favor, llena los campos.
+              Por favor, verifica los campos.
             </Alert>
           </Snackbar>
           <Snackbar
