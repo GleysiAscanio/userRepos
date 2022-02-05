dc746c0bd9201375a09723c19d3f95e953232d6e
Gleysi Ascanio <gleysiascanio29@gmail.com>
2022-02-05 13:50:17 -0500

    Routes are organized for the different views of the user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLogin from "./Components/UserLogin.jsx";
 import UserRegister from "./Components/UserRegister.jsx";
+import ViewRepositories from "./Components/ViewRepositories.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<UserLogin />} />
-          <Route path="/user-register" element={<UserRegister />} />
+          <Route path="/" element={<UserRegister />} />
+          <Route path="user-login" element={<UserLogin />} />
+          <Route path="view-repositories" element={<ViewRepositories />} />
         </Routes>
       </BrowserRouter>
     </div>
