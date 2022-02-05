dddf6e1bcd3964737e8bb78f1786ed1705e81331
Gleysi Ascanio <gleysiascanio29@gmail.com>
2022-02-05 15:51:06 -0500

    New routes are added and others are modified, according to the needs of the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLogin from "./Components/UserLogin.jsx";
 import UserRegister from "./Components/UserRegister.jsx";
 import ViewRepositories from "./Components/ViewRepositories.jsx";
+import ViewFavorites from "./Components/ViewFavorites.jsx";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<UserRegister />} />
           <Route path="user-login" element={<UserLogin />} />
-          <Route path="view-repositories" element={<ViewRepositories />} />
+          <Route path="/view-repositories" element={<ViewRepositories />} />
+          <Route path="/view-favorites" element={<ViewFavorites />} />
         </Routes>
       </BrowserRouter>
     </div>
